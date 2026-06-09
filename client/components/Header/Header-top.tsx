bb392cd9014a3a655a57aa649471259dc4c5fce1
Birzhan <birgan1985@mail.ru>
2026-06-09 14:58:15 +0500

    Исправил баг перевода страниц

--- client/components/Header/Header-top.tsx
+++ client/components/Header/Header-top.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { useTranslations } from "next-intl";
+import { useLocale, useTranslations } from "next-intl";
 import { Button } from "@/components/ui/button";
 import LanguageSwitcher from "../LanguageSwitcher/LanguageSwitcher";
 
@@ -12,30 +12,31 @@ const navLinks = [
 ];
 
 const HeaderTop = () => {
+    const locale = useLocale();
     const t = useTranslations("headerTop");
 
     return (
         <section className="max-w-7xl mx-auto w-full px-10 py-4 flex items-center justify-between">
-            <Link href="/" className="flex items-center gap-2 font-bold text-2xl text-blue-900">
+            <Link href={`/${locale}`} className="flex items-center gap-2 font-bold text-2xl text-blue-900">
                 User Management
             </Link>
 
             <nav className="hidden md:flex items-center gap-8 text-gray-700 font-medium">
                 {navLinks.map((link) => (
-                    <Link key={link.name} href={link.href} className="hover:text-blue-600">
+                    <Link key={link.name} href={`/${locale}${link.href}`} className="hover:text-blue-600">
                         {t(link.name)}
                     </Link>
                 ))}
             </nav>
 
             <div className="flex items-center gap-4">
-                <Link href="/login">
+                <Link href={`/${locale}/login`}>
                     <Button variant="outline" size="lg">
                         {t("login")}
                     </Button>
                 </Link>
 
-                <Link href="/register">
+                <Link href={`/${locale}/register`}>
                     <Button variant="primary" size="lg">
                         {t("signup")}
                     </Button>
